4af128fec42913a6ed0046c4472bd2fc402cd408
wanggang <360507245@qq.com>
2015-01-14 19:16:08 +0800

    1

--- src/app/roles/Player.lua
+++ src/app/roles/Player.lua
@@ -6,14 +6,18 @@ function Player:ctor()
     
 end
 
-function Player:addDajiAnimation()
-    local animationNames = {"walk", "attack", "dead"}
-    local animationFrameNum = {4, 4, 4}
-
-    for i = 1, #animationNames do
+function Player:addAnimation()
+    local animationNames = {"walk","attack","dead"}
+    local animationFrameNum = {4,4,4}
+    
+    for i = 1,#animationNames do
         local frames = display.newFrames("player1-" .. i .. "-%d.png", 1, animationFrameNum[i])
-        local animation = display.newAnimation(frames, 0.2)
+        local animation = display.newAnimation(Frames,0.2)
         display.setAnimationCache("player1-" .. animationNames[i], animation)
     end
+end
+
+function Player:attack()
+    transition.playAnimationOnce(self, display.getAnimationCache("player1-attack"))
 end
 return Player
--- src/app/scenes/MainScene.lua
+++ src/app/scenes/MainScene.lua
@@ -5,6 +5,9 @@ local MainScene = class("MainScene", function()
 end)
 
 function MainScene:ctor()
+    --加入缓存动画
+    Player:addAnimation();
+
     --加入背景
     local background = display.newSprite("image/background.png",display.cx,display.cy)
     self:addChild(background)
@@ -18,6 +21,9 @@ function MainScene:ctor()
     self.enemy = Enemy.new()
     self.enemy:setPosition(display.right - self.enemy:getContentSize().width/2, display.cy)
     self:addChild(self.enemy)
+    
+    --加入触屏事件
+    self:addTouchLayer()
 end
 
 function MainScene:onEnter()
@@ -26,5 +32,20 @@ end
 function MainScene:onExit()
 end
 
+function MainScene:addTouchLayer()
+    local function onTouch(eventName,x,y)
+        if eventName == "began" then
+            Player:attack()
+        end
+    end
+    --new一个可接受点击的节点
+    self.layerTouch = display.newLayer()
+    --给节点添加点击回调函数
+    self.layerTouch:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
+        return onTouch(event.name, event.x, event.y)
+    end)
+    --将此节点加入到游戏中
+    self:addChild(self.layerTouch)
+end
 
 return MainScene
